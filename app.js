ab9a92919d85d4d58a8a47caef022fde8d1e9cf0
wiiikiii <waakaa@gmail.com>
2020-10-08 09:59:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,233 +1,242 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const scoreDisplay = document.getElementById("score");
+  const palyground = document.getElementById("playground");
 
-  const scoreDisplay = document.getElementById('score')
-  const width = 28
-  let score = 0
-  const grid = document.querySelector('.grid')
+  let score = 0;
+  const width = 28;
+
+  let ghosts = [];
+  let pac_man = null;
+  let current_key = null;
+  let timerId = null;
+
+  const directions = {
+    l: { key: 'l', oposite: 'r' },
+    r: { key: 'r', oposite: 'l' },
+    u: { key: 'u', oposite: 'd' },
+    d: { key: 'd', oposite: 'u' },
+  }
+  const field_counts = {
+    power: { value: 10 },
+    pac_dot: { value: 1 },
+  }
+  // order matters! index is relevant
+  const field_codes = ["pac_dot", "wall", "ghost-area", "power", "empty"];
+  const moveable_neighbors = ["ghost-area", "pac_dot", "power", "empty"];
+  const available_ghosts = ["blinky", "pinky", "inky", "clyde"];
+  const squares = [];
+
+  let game_done = false
+
+  // 0 = pac_dots
+  // 1 = wall
+  // 2 = ghost-area
+  // 3 = power
+  // 4 = empty
   const layout = [
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
-    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
-    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
-    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
-    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
-    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
-    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
-    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
-    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-  ]
-  // 0 - pac-dots
-  // 1 - wall
-  // 2 - ghost-lair
-  // 3 - power-pellet
-  // 4 - empty
+    "1111111111111111111111111111",
+    "1000000000000110000000000001",
+    "1011110111110110111110111101",
+    "1311110111110110111110111131",
+    "1011110111110110111110111101",
+    "1000000000000000000000000001",
+    "1011110111111111111110111101",
+    "1011110111111111111110111101",
+    "1000000000000110000000000001",
+    "1111110111110110111110111111",
+    "1111110114444444444110111111",
+    "1111110114111221114110111111",
+    "1111110114122222214110111111",
+    "4444440004122222214000444444",
+    "1111110114122222214110111111",
+    "1111110114122222214110111111",
+    "1111110114111111114110111111",
+    "1000000004444444444000000001",
+    "1011110111110110111110111101",
+    "1011110111110110111110111101",
+    "1300110000000000000000110031",
+    "1110110110111111110110110111",
+    "1110110110111111110110110111",
+    "1000000110000110000110000001",
+    "1011111111110110111111111101",
+    "1011111111110110111111111101",
+    "1000000000000000000000000001",
+    "1111111111111111111111111111",
+  ];
 
-  const squares = []
-
-  //create your board
   function createBoard() {
-    for (let i = 0; i < layout.length; i++) {
-      const square = document.createElement('div')
-      grid.appendChild(square)
-      squares.push(square)
+    layout.forEach((line) => {
+      line.split("").forEach((field) => {
+        const square = document.createElement("div");
+        squares.push(square);
 
-      //add layout to the board
-      if(layout[i] === 0) {
-        squares[i].classList.add('pac-dot')
-      } else if (layout[i] === 1) {
-        squares[i].classList.add('wall')
-      } else if (layout[i] === 2) {
-        squares[i].classList.add('ghost-lair')
-      } else if (layout[i] === 3) {
-        squares[i].classList.add('power-pellet')
-      }
-    }
-  }
-  createBoard()
-
-
-  //create Characters
-  //draw pacman onto the board
-  let pacmanCurrentIndex = 490
-  squares[pacmanCurrentIndex].classList.add('pac-man')
-  //get the coordinates of pacman on the grid with X and Y axis
-  // function getCoordinates(index) {
-  //   return [index % width, Math.floor(index / width)]
-  // }
-
-  // console.log(getCoordinates(pacmanCurrentIndex))
-
-  //move pacman
-  function movePacman(e) {
-    squares[pacmanCurrentIndex].classList.remove('pac-man')
-    switch(e.keyCode) {
-      case 37:
-        if(
-          pacmanCurrentIndex % width !== 0 &&
-          !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
-          !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')
-          )
-        pacmanCurrentIndex -= 1
-        if (squares[pacmanCurrentIndex -1] === squares[363]) {
-          pacmanCurrentIndex = 391
-        }
-        break
-      case 38:
-        if(
-          pacmanCurrentIndex - width >= 0 &&
-          !squares[pacmanCurrentIndex -width].classList.contains('wall') &&
-          !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')
-          ) 
-        pacmanCurrentIndex -= width
-        break
-      case 39:
-        if(
-          pacmanCurrentIndex % width < width - 1 &&
-          !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
-          !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')
-        )
-        pacmanCurrentIndex += 1
-        if (squares[pacmanCurrentIndex +1] === squares[392]) {
-          pacmanCurrentIndex = 364
-        }
-        break
-      case 40:
-        if (
-          pacmanCurrentIndex + width < width * width &&
-          !squares[pacmanCurrentIndex +width].classList.contains('wall') &&
-          !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')
-        )
-        pacmanCurrentIndex += width
-        break
-    }
-    squares[pacmanCurrentIndex].classList.add('pac-man')
-    pacDotEaten()
-    powerPelletEaten()
-    checkForGameOver()
-    checkForWin()
-  }
-  document.addEventListener('keyup', movePacman)
-
-  // what happens when you eat a pac-dot
-  function pacDotEaten() {
-    if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
-      score++
-      scoreDisplay.innerHTML = score
-      squares[pacmanCurrentIndex].classList.remove('pac-dot')
-    }
+        playground.appendChild(square);
+        square.classList.add(field_codes[field]);
+      });
+    });
   }
 
-  //what happens when you eat a power-pellet
-  function powerPelletEaten() {
-    if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
-      score +=10
-      ghosts.forEach(ghost => ghost.isScared = true)
-      setTimeout(unScareGhosts, 10000)
-      squares[pacmanCurrentIndex].classList.remove('power-pellet')
-    }
+  function movableFields() {
+    return squares.filter((f) => {
+      return moveable_neighbors.includes(f.classList.item(0));
+    });
   }
 
-  //make the ghosts stop flashing
-  function unScareGhosts() {
-    ghosts.forEach(ghost => ghost.isScared = false)
-  }
-
-  //create ghosts using Constructors
   class Ghost {
-    constructor(className, startIndex, speed) {
-      this.className = className
-      this.startIndex = startIndex
-      this.speed = speed
-      this.currentIndex = startIndex
-      this.isScared = false
-      this.timerId = NaN
+    constructor(class_name, current_index, speed = 500) {
+      this.class_name = class_name;
+      this.current_index = current_index;
+      this.speed = speed;
+      this.scarying = false;
+      this.direction = '';
     }
   }
 
-  //all my ghosts
-  ghosts = [
-    new Ghost('blinky', 348, 250),
-    new Ghost('pinky', 376, 400),
-    new Ghost('inky', 351, 300),
-    new Ghost('clyde', 379, 500)
-    ]
+  class PacMan {
+    constructor(current_index) {
+      this.current_index = current_index;
+      this.alive = true
+    }
+  }
 
-  //draw my ghosts onto the grid
-  ghosts.forEach(ghost => {
-    squares[ghost.currentIndex].classList.add(ghost.className)
-    squares[ghost.currentIndex].classList.add('ghost')
+  // filter ghosts from field class lists
+  function filter_elements_classnames( class_list ) {
+    let ret = []
+    class_list.forEach( (c) => {
+      if( field_codes.includes( c ) ) {
+        ret.push(c)
+      }
     })
-
-  //move the Ghosts randomly
-  ghosts.forEach(ghost => moveGhost(ghost))
-
-  function moveGhost(ghost) {
-    const directions =  [-1, +1, width, -width]
-    let direction = directions[Math.floor(Math.random() * directions.length)]
-
-    ghost.timerId = setInterval(function() {
-      //if the next squre your ghost is going to go to does not have a ghost and does not have a wall
-      if  (!squares[ghost.currentIndex + direction].classList.contains('ghost') &&
-        !squares[ghost.currentIndex + direction].classList.contains('wall') ) {
-          //remove the ghosts classes
-          squares[ghost.currentIndex].classList.remove(ghost.className)
-          squares[ghost.currentIndex].classList.remove('ghost', 'scared-ghost')
-          //move into that space
-          ghost.currentIndex += direction
-          squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
-      //else find a new random direction ot go in
-      } else direction = directions[Math.floor(Math.random() * directions.length)]
-
-      //if the ghost is currently scared
-      if (ghost.isScared) {
-        squares[ghost.currentIndex].classList.add('scared-ghost')
-      }
-
-      //if the ghost is currently scared and pacman is on it
-      if(ghost.isScared && squares[ghost.currentIndex].classList.contains('pac-man')) {
-        squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
-        ghost.currentIndex = ghost.startIndex
-        score +=100
-        squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
-      }
-    checkForGameOver()
-    }, ghost.speed)
+    return ret
   }
 
-  //check for a game over
-  function checkForGameOver() {
-    if (squares[pacmanCurrentIndex].classList.contains('ghost') &&
-      !squares[pacmanCurrentIndex].classList.contains('scared-ghost')) {
-      ghosts.forEach(ghost => clearInterval(ghost.timerId))
-      document.removeEventListener('keyup', movePacman)
-      setTimeout(function(){ alert("Game Over"); }, 500)
+  // finding fields around current position
+  // easy find possible next fields for ghost and pac man
+  function find_neighbors(current_pos) {
+    let neighbors = {};
+    let tmp_pos = current_pos % width == 0 ? current_pos + width - 1 : current_pos - 1;
+    neighbors.l = { dir: 'l', pos: tmp_pos, ftype: filter_elements_classnames( squares[tmp_pos].classList )[0] || 'empty' };
+
+    tmp_pos = current_pos % width == width - 1 ? current_pos - width + 1 : current_pos + 1;
+    neighbors.r = { dir: 'r', pos: tmp_pos, ftype: filter_elements_classnames( squares[tmp_pos].classList )[0] || 'empty' };
+
+    neighbors.u = { dir: 'u', pos: current_pos - width, ftype: filter_elements_classnames( squares[current_pos - width].classList )[0] || 'empty' };
+    neighbors.d = { dir: 'd', pos: current_pos + width, ftype: filter_elements_classnames( squares[current_pos + width].classList )[0] || 'empty' };
+    return neighbors;
+  }
+
+  function count_and_mark_field( idx ) {
+    let field = squares[ idx ]
+    counts = field_counts[ field.classList.item(0) ]
+    if( counts && counts.value > 0 ) {
+      score += counts.value
+      field.classList.remove( field.classList.item(0) )
+      scoreDisplay.innerHTML = score
     }
   }
 
-  //check for a win - more is when this score is reached
-  function checkForWin() {
-    if (score === 274) {
-      ghosts.forEach(ghost => clearInterval(ghost.timerId))
-      document.removeEventListener('keyup', movePacman)
-      setTimeout(function(){ alert("You have WON!"); }, 500)
+  function check_collision_with_ghost( pac_man_pos ) {
+    let done = ghosts.find( (g) => {
+      return g.current_index == pac_man_pos
+    })
+    if( done ) {
+      clearInterval( timerId )
+
+      scoreDisplay.innerHTML = `${ score } - Game is done`
     }
   }
-})
+
+  function movePacMan() {
+    let old_index = pac_man.current_index;
+    let neighbors = find_neighbors(old_index);
+    if ( current_key && moveable_neighbors.includes( neighbors[current_key].ftype ) ) {
+      pac_man.current_index = neighbors[current_key].pos;
+    }
+    if (!current_key || old_index !== pac_man.current_index) {
+      squares[old_index].classList.remove("pac-man");
+      squares[pac_man.current_index].classList.add("pac-man");
+
+      count_and_mark_field( pac_man.current_index )
+    }
+  }
+
+  function moveGhosts() {
+    ghosts.forEach((g) => {
+      let neighbors = find_neighbors(g.current_index);
+      let old_pos = g.current_index
+
+      if( g.direction == '' ) {
+        g.direction = ["l", "r", "u", "d"][Math.floor(Math.random() * 3)];
+      }
+
+      let possible_directions = Object.values(neighbors).filter((d) => {
+        return moveable_neighbors.includes(d.ftype);
+      });
+
+      let filtered = possible_directions.filter((d) => d.dir != directions[ g.direction ].oposite)
+
+      let nxt = null
+      console.log( g.class_name, possible_directions, filtered, filtered.length )
+      if ( filtered.length > 1 ) {
+        nxt = filtered[ Math.floor( Math.random() * filtered.length ) ]
+      } else {
+        nxt = filtered[0]
+        if( ! nxt ) { possible_directions[ 0 ]}
+      }
+      console.log( nxt )
+      try {
+        g.direction = nxt.dir;
+        g.current_index = nxt.pos;
+      } catch (e) {}
+
+      squares[old_pos].classList.remove(g.class_name);
+      squares[g.current_index].classList.add(g.class_name);
+    });
+  }
+
+  function render() {
+    movePacMan();
+    check_collision_with_ghost(pac_man.current_index);
+    moveGhosts();
+    check_collision_with_ghost(pac_man.current_index);
+  }
+
+  function startGame() {
+    createBoard();
+    let af = movableFields();
+
+    available_ghosts.forEach((f) => {
+      let current_pos = Math.floor(Math.random() * af.length + 0.5);
+      ghosts.push(new Ghost(f, squares.indexOf(af[current_pos])));
+    });
+
+    // let current_pos = Math.floor(Math.random() * af.length + 0.5);
+    // ghosts.push(new Ghost('blinky', squares.indexOf(af[current_pos])));
+
+    let pac_man_pos = Math.floor(Math.random() * af.length + 0.5);
+    pac_man = new PacMan(squares.indexOf(af[pac_man_pos]));
+
+    document.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          current_key = "l";
+          break;
+        case "ArrowRight":
+          current_key = "r";
+          break;
+        case "ArrowUp":
+          current_key = "u";
+          break;
+        case "ArrowDown":
+          current_key = "d";
+          break;
+        default:
+          break;
+      }
+    });
+
+    timerId = setInterval(() => render(), 500);
+  }
+
+  startGame();
+});
